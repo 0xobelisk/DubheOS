83bced2790a50abe3b5ce85f2af3ba043a6eb2f0
web3olalala <web3bob.dev@gmail.com>
2025-03-22 22:40:29 +0800

    Add dubhe token properties

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -4,15 +4,25 @@ use solochain_template_runtime::WASM_BINARY;
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec;
 
+fn dubhe_properties() -> Properties {
+	let mut properties = sc_chain_spec::Properties::new();
+
+	properties.insert("tokenSymbol".into(), "DUBHE".into());
+	properties.insert("tokenDecimals".into(), 7.into());
+
+	properties
+}
+
 pub fn development_chain_spec() -> Result<ChainSpec, String> {
 	Ok(ChainSpec::builder(
 		WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?,
 		None,
 	)
-	.with_name("Development")
+	.with_name("Dubhe OS Node")
 	.with_id("dev")
 	.with_chain_type(ChainType::Development)
 	.with_genesis_config_preset_name(sp_genesis_builder::DEV_RUNTIME_PRESET)
+	.with_properties(dubhe_properties())
 	.build())
 }
 
